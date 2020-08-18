5d9c142b675f0fcd3b1a9ff407006f96cf101df1
Quality <56773210+easternpromises@users.noreply.github.com>
2020-08-18 12:54:49 -0700

    Update app.js
    When clicking the background now the '@' is removed.

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 let input = document.getElementsByClassName("custom-input");
+const page = document.querySelector('body');
+
 
 $("input").focus(function(){
     $(input).val("@");
   });
+
+page.addEventListener('click', function(e){
+  $(input).val("");
+});
